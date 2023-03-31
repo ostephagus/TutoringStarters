9ae01e6fd08d363d44ad5b1fb498209c01c8b2eb
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-03-31 10:18:37 +0100

    Added forgetting model, changed serialisation

--- TutoringStarters/Program.cs
+++ TutoringStarters/Program.cs
@@ -1,39 +1,36 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
+﻿using System.Security.Cryptography.X509Certificates;
 
 namespace TutoringStarters
 {
+    //PLAN:
+    //Each topic has a Topic object, that stores the current level of remembering.
+    //A handler will manage ensuring that the Topics are up to date, since the level of remembering is a function of time and difficulty
+    //Topics will also have their own serialisation method, to store the topics and a "week zero" which will be used to calculate the time for the remembering level function.
+    //For finding topics to revisit, a subroutine searches the Topics for the lowest remembering level.
     internal class Program
     {
         static void Main(string[] args)
         {
-            DateTime weekZero = new DateTime(2023, 03, 27);
-            Console.WriteLine(CalculateWeekOffset(DateTime.Today, weekZero));
-            
-        }
+            (DateTime weekZero, Topic[] topics) = FileHandler.Read("topics.bin");
 
-        static int CalculateWeekOffset(DateTime currentDay, DateTime weekZero)
-        {
-            DateTime nextMonday = currentDay.AddDays((8-(int)currentDay.DayOfWeek) % 7); //Calculation to find the next monday
-            return (int)(nextMonday - weekZero).TotalDays / 7; //Find the difference in days and divide by 7 to get weeks.
         }
-
     }
 
     public static class FileHandler
     {
-        static void Save(Topic[] topics, int weekCounter, string filename)
+        public static void Save(DateTime weekZero, Topic[] topics, string filename)
         {
             ///<summary>
             ///Serialises and saves Topic objects to the file at <paramref name="filename" />. Creates the file if it does not already exist.
             ///</summary>
+            ///<param name="weekZero">The date of the week used as a baseline for time calculations</param>
             ///<param name="topics">The array of Topic objects to save</param>
             FileStream outputStream = new FileStream(filename, FileMode.Create, FileAccess.Write);
-            BinaryFormatter formatter = new BinaryFormatter();
             //Need to implement binary serialisation
 
         }
 
-        static (Topic[], int) Read(string filename)
+        public static (DateTime, Topic[]) Read(string filename)
         {
             /// <summary>
             /// Reads in a serialised binary file and return its constituent Topic objects.
@@ -50,26 +47,36 @@ namespace TutoringStarters
     {
         private string name;
         private int difficulty;
-        private int score;
+        private DateTime creationDate;
 
         public string Name { get => name; set => name = value; }
         public int Difficulty { get => difficulty; set => difficulty = value; }
-        public int Score { get => score; set => score = value; }
+        public double RememberingLevel { get => CalculateRememberingLevel(); }
 
+        public Topic(string name, int difficulty, DateTime creationDate)
+        {
+            this.name = name;
+            this.difficulty = difficulty;
+            this.creationDate = creationDate;
+        }
         public Topic(string name, int difficulty)
         {
             this.name = name;
             this.difficulty = difficulty;
-            score = 0;
+            creationDate = DateTime.Today;
         }
 
-        public Topic(string name, int difficulty, int score)
+        private int CalculateWeekOffset()
         {
-            this.name = name;
-            this.difficulty = difficulty;
-            this.score = score;
+            DateTime today = DateTime.Today;
+            DateTime nextMonday = today.AddDays((8 - (int)today.DayOfWeek) % 7); //Calculation to find the next monday
+            return (int)(nextMonday - creationDate).TotalDays / 7; //Find the difference in days and divide by 7 to get weeks.
         }
 
-        //Use of a sigmoid function, possibly 1/(1+e^(kx-4)), to model forgetting over time
+        private double CalculateRememberingLevel()
+        {
+            //Use of modified sigmoid logistic function, 1/(1+2^(kt-6))
+            return 1 / (1 + Math.Pow(2, difficulty * CalculateWeekOffset() - 6));
+        }
     }
 }
